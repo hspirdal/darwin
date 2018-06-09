a09756773b38f711484eb16c9ab9add52e0edccc
Halvor Spirdal <hspirdal@gmail.com>
2018-06-09 12:46:15 +0200

    Add simple logging shell.

---
+++ src/TcpGameServer/Logging/Logger.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace TcpGameServer.Logging
+{
+	public interface ILogger
+	{
+		void Debug(string message);
+		void Info(string message);
+		void Warning(string message);
+		void Error(Exception exception);
+	}
+
+	public class Logger : ILogger
+	{
+		public void Debug(string message)
+		{
+			System.Diagnostics.Debug.WriteLine(message);
+		}
+
+		public void Info(string message)
+		{
+			Console.WriteLine(message);
+		}
+
+		public void Warning(string message)
+		{
+			Console.WriteLine($"WARNING - {message}");
+		}
+
+		public void Error(Exception exception)
+		{
+			var innermostException = TraverseInnermostException(exception);
+			Debug(innermostException.Message);
+			Debug(innermostException.StackTrace);
+		}
+
+		private static Exception TraverseInnermostException(Exception exception)
+		{
+			if (exception.InnerException != null)
+			{
+				return TraverseInnermostException(exception);
+			}
+			return exception;
+		}
+	}
+}
