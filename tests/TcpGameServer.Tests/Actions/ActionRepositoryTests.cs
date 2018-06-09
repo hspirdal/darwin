f1d79bcef2fbd43eb042869c52e0ae7f260d6dd9
Halvor Spirdal <hspirdal@gmail.com>
2018-06-09 12:23:12 +0200

    Add some more tests to action repository.

--- tests/TcpGameServer.Tests/Actions/ActionRepositoryTests.cs
+++ tests/TcpGameServer.Tests/Actions/ActionRepositoryTests.cs
@@ -17,25 +17,35 @@ namespace TcpGameServer.Tests.Actions
 		}
 
 		[TestMethod]
-		public void WhenAddingOneActionToEmptyRepository_ThenCountIsOne()
+		public void WhenPushingOneActionIntoEmptyRepository_ThenRepositoryCountIsOne()
 		{
 			var action = new Action();
 
-			_actionRepository.EnqueueAction(action);
-			var actions = _actionRepository.DequeueActions();
+			_actionRepository.PushInto(action);
+			var currentCount = _actionRepository.Count();
 
-			Assert.AreEqual(1, actions.Count);
+			Assert.AreEqual(1, currentCount);
 		}
 
+		[TestMethod]
+		public void WhenPullingOutActions_ThenTheyAreRemovedFromRepository()
+		{
+			var expectedCount = _actionRepository.Count();
 
+			_actionRepository.PushInto(new Action());
+			_actionRepository.PullOut();
+			var currentCount = _actionRepository.Count();
+
+			Assert.AreEqual(expectedCount, currentCount);
+		}
 
 		[TestMethod]
-		public void WhenAddingActionsConcurrently_ThenAllActionsAreInsertedCorrectly()
+		public void WhenPushingActionsConcurrently_ThenAllActionsAreInsertedEventually()
 		{
-			var threadCount = 500;
-			var tasks = new Task[threadCount];
+			var concurrentActionCount = 500;
+			var tasks = new Task[concurrentActionCount];
 			var nextId = 0;
-			for (var i = 0; i < threadCount; ++i)
+			for (var i = 0; i < concurrentActionCount; ++i)
 			{
 				tasks[i] = Task.Run(() =>
 				{
@@ -46,14 +56,46 @@ namespace TcpGameServer.Tests.Actions
 
 			Task.WaitAll(tasks);
 
-			var actions = _actionRepository.DequeueActions();
-			Assert.AreEqual(threadCount, actions.Count);
+			var actions = _actionRepository.PullOut();
+			Assert.AreEqual(concurrentActionCount, actions.Count);
+		}
+
+		[TestMethod]
+		public void WhenPullingOutActions_ThenClientsCanStillAddActionsConcurrently()
+		{
+			var concurrentActionCount = 500;
+			var tasks = new Task[concurrentActionCount];
+			var nextId = 0;
+			var firstBatchCount = 0;
+			for (var i = 0; i < concurrentActionCount; ++i)
+			{
+				tasks[i] = Task.Run(() =>
+				{
+					Interlocked.Add(ref nextId, 1);
+					InsertOneArbitraryAction(nextId);
+
+					if (nextId == 100)
+					{
+						var firstBatchOfActions = _actionRepository.PullOut();
+						firstBatchCount = firstBatchOfActions.Count;
+					}
+				});
+			}
+
+			Task.WaitAll(tasks);
+
+			var secondBatchOfActions = _actionRepository.PullOut();
+			var secondBatchCount = secondBatchOfActions.Count;
+			var totalActionCount = firstBatchCount + secondBatchCount;
+
+			Assert.AreEqual(totalActionCount, concurrentActionCount);
+			Assert.IsTrue(firstBatchCount > 0);
 		}
 
 		private void InsertOneArbitraryAction(int id)
 		{
 			var action = new Action { OwnerId = id };
-			_actionRepository.EnqueueAction(action);
+			_actionRepository.PushInto(action);
 		}
 	}
 }
