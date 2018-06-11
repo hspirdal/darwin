2a350ab7c3147d233fb4b013d5aadcd062b25313
Halvor Spirdal <hspirdal@gmail.com>
2018-06-11 23:30:08 +0200

    Add some temporary render code for dungeon map.

--- src/TcpGameClient/GameClient.cs
+++ src/TcpGameClient/GameClient.cs
@@ -1,39 +1,62 @@
 using Ether.Network.Client;
 using Ether.Network.Packets;
+using Newtonsoft.Json;
 using System;
 using System.Net.Sockets;
+using TcpGameServer.Contracts.Area;
 
 namespace TcpGameClient
 {
-    public class GameClient : NetClient
-    {
-        public GameClient(string host, int port, int bufferSize, int timeOut)
-        {
-            Configuration.Host = host;
-            Configuration.Port = port;
-            Configuration.BufferSize = bufferSize;
-            //Configuration.TimeOut = timeOut;
-        }
+	public class GameClient : NetClient
+	{
+		public GameClient(string host, int port, int bufferSize, int timeOut)
+		{
+			Configuration.Host = host;
+			Configuration.Port = port;
+			Configuration.BufferSize = bufferSize;
+			//Configuration.TimeOut = timeOut;
+		}
 
-        public override void HandleMessage(INetPacketStream packet)
-        {
-            var response = packet.Read<string>();
-            Console.WriteLine($"-> Server response: '{response}'");
-        }
+		public override void HandleMessage(INetPacketStream packet)
+		{
+			var json = packet.Read<string>();
+			var status = JsonConvert.DeserializeObject<StatusResponse>(json);
 
-        protected override void OnConnected()
-        {
-            Console.WriteLine("Connected to {0}", this.Socket.RemoteEndPoint);
-        }
+			Console.SetCursorPosition(0, 0);
 
-        protected override void OnDisconnected()
-        {
-            Console.WriteLine("Disconnected");
-        }
+			if (status != null)
+			{
+				for (var y = 0; y < status.Map.Height; ++y)
+				{
+					for (var x = 0; x < status.Map.Width; ++x)
+					{
+						var cell = status.Map.GetCell(x, y);
+						var cellSymbol = cell.IsWalkable ? ' ' : '#';
+						if (status.X == x && status.Y == y)
+						{
+							cellSymbol = '@';
+						}
 
-        protected override void OnSocketError(SocketError socketError)
-        {
-            Console.WriteLine("Socket Error: {0}", socketError.ToString());
-        }
-    }
+						Console.Write(cellSymbol);
+					}
+					Console.Write(Environment.NewLine);
+				}
+			}
+		}
+
+		protected override void OnConnected()
+		{
+			Console.WriteLine("Connected to {0}", this.Socket.RemoteEndPoint);
+		}
+
+		protected override void OnDisconnected()
+		{
+			Console.WriteLine("Disconnected");
+		}
+
+		protected override void OnSocketError(SocketError socketError)
+		{
+			Console.WriteLine("Socket Error: {0}", socketError.ToString());
+		}
+	}
 }
