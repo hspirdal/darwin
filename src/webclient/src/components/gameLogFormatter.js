44387d9d2b22e5e89b25ee93e8a061867ab0dfbe
Halvor Spirdal <hspirdal@gmail.com>
2018-07-31 02:01:20 +0200

    Attempt to level up my javascript game.

--- src/webclient/src/components/gameLogFormatter.js
+++ src/webclient/src/components/gameLogFormatter.js
@@ -1,15 +1,17 @@
+"use strict";
+
 export default {
 	formatSuccessfulHit(player, attackResult) {
-		var name = this.formatName(player, attackResult);
-		var damage = this.formatDamage(attackResult);
-		var toHit = this.formatToHit(attackResult);
-		return name + ": Attack " + attackResult.DefenderName + " for " + damage + " " + toHit;
+		let name = this.formatName(player, attackResult);
+		let damage = this.formatDamage(attackResult);
+		let toHit = this.formatToHit(attackResult);
+		return `${name}: Attack ${attackResult.DefenderName} for ${damage} ${toHit}`;
 	},
 
 	formatFailedHit(player, attackResult) {
-		var name = this.formatName(player, attackResult);
-		var toHit = this.formatToHit(attackResult);
-		return name + ": Fail to attack " + attackResult.DefenderName + " " + toHit;
+		let name = this.formatName(player, attackResult);
+		let toHit = this.formatToHit(attackResult);
+		return `${name}: Fail to attack ${attackResult.DefenderName} ${toHit}`;
 	},
 
 	formatAttacking() {
@@ -17,34 +19,34 @@ export default {
 	},
 
 	formatKilledBy(player, attackResult) {
-		return '<span id="other">' + attackResult.AttackerName + " kills " + player.Name + " </span> - Game over, buddy!";
+		return `<span id="other"> ${attackResult.AttackerName} kills ${player.Name}</span> - Game over, buddy!`;
 	},
 
 	formatKilledOther(attackResult) {
-		return attackResult.DefenderName + " dies of combat damage!";
+		return `${attackResult.DefenderName} dies of combat damage!`;
 	},
 
 	formatExperienceGain(amount) {
-		return '<span id="xpgain">Received ' + amount + " experience points</span> (in a future build).";
+		return `<span id="xpgain">Received ${amount} experience points</span> (in a future build).`;
 	},
 
 	formatMovementFailed(message) {
-		return '<span style="color: blue">' + message + "</span>";
+		return `<span id="failedaction">${message}</span>`;
 	},
 
 	formatName(player, attackResult) {
 		if (player.Name === attackResult.AttackerName) {
-			return '<span id="self">' + player.Name + "</span>";
+			return `<span id="self">${player.Name}</span>`;
 		}
 
-		return '<span id="other">' + attackResult.AttackerName + "</span>";
+		return `<span id="other">${attackResult.AttackerName}</span>`;
 	},
 
 	formatDamage(attackResult) {
-		return '<span id="damage">' + attackResult.DamageTotal + "</span> damage!";
+		return `<span id="damage">${attackResult.DamageTotal}</span> damage!`;
 	},
 
 	formatToHit(attackResult) {
-		return "<i>(ToHit " + attackResult.ToHitTotal + ")</i>";
+		return `<i>(ToHit ${attackResult.ToHitTotal})</i>`;
 	}
 };
