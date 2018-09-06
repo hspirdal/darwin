ebab3a044f7b6d4cfbce5e39b8e6370ca3935be1
Halvor Spirdal <hspirdal@gmail.com>
2018-09-06 22:39:08 +0200

    Add some containers to map.

--- src/GameLib/Area/ItemSpawner.cs
+++ src/GameLib/Area/ItemSpawner.cs
@@ -23,9 +23,11 @@ namespace GameLib.Area
 			{
 				var torch = new Item { Name = "Torch", Id = Guid.NewGuid().ToString() };
 				var shovel = new Item { Name = "Shovel", Id = Guid.NewGuid().ToString() };
+				var barrel = new Container { Name = "Barrel", Id = Guid.NewGuid().ToString() };
 				var cell = _playArea.GameMap.GetRandomOpenCell();
 				_playArea.GameMap.Add(cell.X, cell.Y, torch);
 				_playArea.GameMap.Add(cell.X, cell.Y, shovel);
+				_playArea.GameMap.Add(cell.X, cell.Y, barrel);
 			}
 		}
 	}
