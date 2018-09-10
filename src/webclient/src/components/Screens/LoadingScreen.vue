820943fc4f2ac7637054f66e77a36785ccbfda5a
Halvor Spirdal <hspirdal@gmail.com>
2018-09-10 21:41:13 +0200

    Add some animation spinner while connecting to server.

--- src/webclient/src/components/Screens/LoadingScreen.vue
+++ src/webclient/src/components/Screens/LoadingScreen.vue
@@ -1,8 +1,33 @@
 <template>
-	<div id="loadingscreen">
-		<h1>Loading...</h1>
+	<div id="loadingscreen" class="container">
+		<div class="item">
+		<h1>Connecting to server..</h1>
+		<scaling-squares-spinner class="spinner"
+			:animation-duration="1250"
+			:size="165"
+			color="#6B97FF"
+		/>
+		</div>
 	</div>
 </template>
 <script>
-export default {};
+import { ScalingSquaresSpinner } from "epic-spinners";
+
+export default {
+	components: {
+		ScalingSquaresSpinner
+	}
+};
 </script>
+<style scoped>
+.container {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 68vh;
+}
+.spinner {
+	margin: auto;
+	width: 50%;
+}
+</style>
